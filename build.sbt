b1bd88bc537a8611451475c0813521f9419cfe13
Dalen W. Brauner <Dalen.Brauner12@ncf.edu>
2017-02-14 13:50:36 -0500

    Toying with Tests and Logging

--- build.sbt
+++ build.sbt
@@ -5,8 +5,10 @@ val ScalatraVersion = "2.2.2"
 libraryDependencies ++= Seq(
   "org.scalatra" %% "scalatra" % ScalatraVersion,
   "org.scalatra" %% "scalatra-scalate" % ScalatraVersion,
+  "org.scalatra" %% "scalatra-scalatest" % ScalatraVersion % "test",
   "org.scalatra" %% "scalatra-specs2" % ScalatraVersion % "test",
   "org.eclipse.jetty" % "jetty-webapp" % "8.1.7.v20120910" % "container,compile",
-  "org.eclipse.jetty.orbit" % "javax.servlet" % "3.0.0.v201112011016")
+  "org.eclipse.jetty.orbit" % "javax.servlet" % "3.0.0.v201112011016",
+  "ch.qos.logback" % "logback-classic" % "1.0.1")
 
 Seq(webSettings :_*)
--- src/main/scala/sampleApi.controllers/GreetingController.scala
+++ src/main/scala/sampleApi.controllers/GreetingController.scala
@@ -1,15 +1,21 @@
 package sampleApi.controllers
 import org.scalatra.ScalatraServlet
+import org.slf4j.{Logger, LoggerFactory}
 //import scalate.ScalateSupport
 
 //class GreetingController extends ScalatraServlet with ScalateSupport {
 class GreetingController extends ScalatraServlet {
+  val logger = LoggerFactory.getLogger(getClass)
+  logger.info("GreetingController appears operational.")
+
   get("/") {
+    logger.info("Index page contacted.")
     "Hello, Scala(tra)!"
   }
 
   get("/:name") {
     val name = params.getOrElse("name", "World")
     "Hello, " + name + "!"
+    logger.info(s"World $name contacted.")
   }
 }
