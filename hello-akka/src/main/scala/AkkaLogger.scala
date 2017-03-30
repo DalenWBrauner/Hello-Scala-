55672b825120c65f468ff1898365d5a12c0e26d7
dalenwbrauner <DalenWBrauner@gmail.com>
2017-03-30 10:25:07 -0400

    Scala Bang magically works now

--- hello-akka/src/main/scala/AkkaLogger.scala
+++ hello-akka/src/main/scala/AkkaLogger.scala
@@ -39,16 +39,16 @@ object AkkaLogger extends App {
 
   // Let's write Akka a letter...
   val ourID = 0 // We would do this very differently if we were using real actors, but
-  logger.tell(LogThis(ourID,"Dear Akka,"), ActorRef.noSender)
-  logger.tell(LogThis(ourID,"Your tutorial had some pretty bad variable names,"), ActorRef.noSender)
-  logger.tell(LogThis(ourID,"but it still helped me make sense of everything in the end."), ActorRef.noSender)
-  logger.tell(LogThis(ourID,"That activator thing is pretty interesting!"), ActorRef.noSender)
-  logger.tell(LogThis(ourID,"It's definitely eerie that it's automatically compiling everything"), ActorRef.noSender)
-  logger.tell(LogThis(ourID,"I write the minute I hit save and then running it, though."), ActorRef.noSender)
-  logger.tell(LogThis(ourID,"But I guess it saves me from having to type 'sbt run' in the console,"), ActorRef.noSender)
-  logger.tell(LogThis(ourID,"so thank you."), ActorRef.noSender)
-  logger.tell(LogThis(ourID,"\t\tSincere thanks,"), ActorRef.noSender)
-  logger.tell(LogThis(ourID,"\t\tDalen."), ActorRef.noSender)
+  logger ! LogThis(ourID,"Dear Akka,")
+  logger ! LogThis(ourID,"Your tutorial had some pretty bad variable names,")
+  logger ! LogThis(ourID,"but it still helped me make sense of everything in the end.")
+  logger ! LogThis(ourID,"That activator thing is pretty interesting!")
+  logger ! LogThis(ourID,"It's definitely eerie that it's automatically compiling everything")
+  logger ! LogThis(ourID,"I write the minute I hit save and then running it, though.")
+  logger ! LogThis(ourID,"But I guess it saves me from having to type 'sbt run' in the console,")
+  logger ! LogThis(ourID,"so thank you.")
+  logger ! LogThis(ourID,"\t\tSincere thanks,")
+  logger ! LogThis(ourID,"\t\tDalen.")
   inbox.send(logger, LogRequest)
 
   // ...and wait a few seconds for the reply.
